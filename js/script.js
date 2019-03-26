91be3d71caf61729e31d2061f7816fbdcecbe057
hoanghongchuong <chuonghoanhong@gmail.com>
2019-03-26 14:39:54 +0700

    a

--- js/script.js
+++ js/script.js
@@ -24,42 +24,29 @@ $(document).ready(function($){
 		});
 
 		
-	    $('.slider-for').slick({
-		  	slidesToShow: 1,
-		  	slidesToScroll: 1,
-		  	arrows: false,
-		  	fade: true,
-		  	asNavFor: '.slider-nav'
-		});
-		$('.slider-nav').slick({
-		  	slidesToShow: 8,
-		  	slidesToScroll: 1,
-		  	asNavFor: '.slider-for',
-		  	dots: true,
-		  	centerMode: true,
-		  	focusOnSelect: true,
-		  	responsive: [{
-	            breakpoint: 992,
-	            settings: {
-	                slidesToShow: 3,
-	            }
 
-	        },
-            {
-                breakpoint: 568,
-                settings: {
-                    slidesToShow: 3,
-                }
-
-            },
-            {
-                breakpoint: 320,
-                settings: {
-                    slidesToShow: 3,
-                }
-
-            }
-            ]
+		var owl = $(".owl-carousel-slider");
+	  	owl.owlCarousel({
+	  		margin:0, 							
+	  		loop:true, 							
+	  		nav:true, 							
+	  		navText:['<i class="fa fa-caret-left"></i>','<i class="fa fa-caret-right"></i>'], 
+	  		autoplay:false, 						
+	  		autoplayTimeout:1500,
+			autoplayHoverPause:true,
+			autoplaySpeed: 2000,
+			responsiveClass:true, 				
+		    responsive:{
+		        0:{
+		            items:1,									            									            
+		        },
+		        600:{
+		            items:4,          
+		        },
+		        1000:{
+		            items:4,  
+		        }
+		    }
 		});
 
 });
